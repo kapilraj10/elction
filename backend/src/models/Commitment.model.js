899ece5e91bb7966879f8a76e5ab976872068d26
kapilraj10 <kapilrajkc10@gmail.com>
2026-02-02 20:54:27 +0545

    update a code

--- backend/src/models/Commitment.model.js
+++ backend/src/models/Commitment.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const commitmentSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    body: { type: String, required: true },
+    description: { type: String, required: true },
     published: { type: Boolean, default: false },
     order: { type: Number, default: 0 },
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
